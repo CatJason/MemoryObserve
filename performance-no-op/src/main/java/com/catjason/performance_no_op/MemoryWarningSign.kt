7a0dcc4c9c1ca9e328edf4680a6f544e07c7ee25
wangzhijun <zhijun.wang@maxjia.com>
2023-12-21 10:45:38 +0800

    Release-no-op

--- performance-no-op/src/main/java/com/catjason/performance_no_op/MemoryWarningSign.kt
+++ performance-no-op/src/main/java/com/catjason/performance_no_op/MemoryWarningSign.kt
@@ -1,4 +1,13 @@
 package com.catjason.performance_no_op
 
+import android.content.Context
+
 object MemoryWarningSign {
+    fun show(context: Context) {
+        // Do Nothing
+    }
+
+    fun hide(context: Context) {
+        // Do Nothing
+    }
 }
