ef129b8a996ba7b0039af00d718c10394124ead0
wangzhijun <zhijun.wang@maxjia.com>
2023-12-21 12:56:19 +0800

    Release fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,8 @@ android {
 }
 
 dependencies {
-    implementation(project(mapOf("path" to ":performance")))
+    debugImplementation(project(":performance"))
+    releaseImplementation(project(":performance-no-op"))
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.8.0")
