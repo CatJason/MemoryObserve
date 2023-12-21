16b626cc243139ca5693898ccc7365e9754ca6f0
wangzhijun <zhijun.wang@maxjia.com>
2023-12-21 13:27:45 +0800

    V3.1-no-op

--- performance/src/main/java/cat/jason/performance/MemoryWarningSign.kt
+++ performance/src/main/java/cat/jason/performance/MemoryWarningSign.kt
@@ -8,30 +8,11 @@ import android.os.Build
 import android.provider.Settings
 
 object MemoryWarningSign {
-    private const val REQUEST_CODE = 100 // 或者任何其他整数
     fun show(context: Context) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(context)) {
-            val intent = Intent(
-                Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
-                Uri.parse("package:${context.packageName}")
-            )
-            if (context is Activity) {
-                // If context is an Activity, use startActivityForResult
-                context.startActivityForResult(intent, REQUEST_CODE)
-            } else {
-                // If it's not an Activity (like a Service or Application context), start the intent without expecting a result
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK) // You need this flag when starting an activity from a non-Activity context
-                context.startActivity(intent)
-            }
-            showPermissionToast(context)
-        } else {
-            context.startService(Intent(context, MemoryService::class.java))
-            showGuideToast(context)
-        }
+
     }
 
     fun hide(context: Context) {
-        context.stopService(Intent(context, MemoryService::class.java))
     }
 }
 
